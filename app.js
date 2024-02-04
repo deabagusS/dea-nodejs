1172cbe0c8a1847bb7da668e127f2692817eabac
deaprieds <dea.bagus@prieds.com>
2024-02-04 10:15:23 +0700

    appjd remove listen

--- app.js
+++ app.js
@@ -28,9 +28,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// app.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
